1c36d200e68777ced560799c895e5b11d43ec664
Codewith-AyushGupta <ayush.gupta@nubystech.com>
2025-06-06 13:45:38 +0530

    changes 0n handler

--- handler.py
+++ handler.py
@@ -4,7 +4,7 @@ import json
 def lambda_handler(event, context):
     print('In Event',event)
     body = {
-        "message": "Go Serverless v4.0! Your function executed successfully!",
+        "message": "Go to Serverless v4.0! Your function executed successfully!",
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
